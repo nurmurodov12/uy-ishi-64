64bfc440b9b46ebf977c164cba3b1b41ba6ddc7c
temurx_2009 <xurshidberdiyev315@gmail.com>
2026-05-08 21:30:53 +0500

    web

---
+++ app.js
@@ -0,0 +1,157 @@
+// 1
+// const mostBig = (a, b, c) => {
+//     if (a > b && a > c) {
+//         return `kiritilgan ${a} soni katta barchasidan`
+//     } else if (b > a && b > c) {
+//         return `kiritilgan ${b} soni katta barchasidan`
+//     }else  {
+//         return `kiritilgan ${c} soni katta barchasidan`
+//     }
+// }
+
+// console.log(mostBig(10, 40, 30));
+
+// 2
+// const isCouple = function (number) {
+//     if (number % 2 !== 0) {
+//         return false
+//     } else {
+//         return true
+//     }
+// }
+
+// console.log(isCouple(22));
+
+// 3
+// const isKabisa = function (year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 400 === 0)) {
+//         return `kiritilgan ${year} yil kabisa yili`
+//     } else {
+//         return `kiritilgan ${year} yil kabisa yili emas`
+
+//     }
+// }
+
+// console.log(isKabisa(2026));
+
+// 4
+// const textReverse = (text) => {
+//     let newText = ""
+//     for (let i = text.length - 1; i >= 0; i--) {
+//         newText += text[i]
+//     }
+
+//     return newText
+// }
+// console.log(textReverse("salom"));
+
+// console.log(textReverse("saloom"));
+
+// 5
+// const countVowel = (text) => {
+//     let counter = 0
+
+//     for (let i of text) {
+//         if ((i === "a" || i === "A") || (i === "e" || i === "E") || (i === "i" || i === "I") || (i === "o" || i === "O") || (i === "u" || i === "U")) {
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+
+
+// console.log(countVowel("salom"));
+
+// 6
+// const massivSum = (array = []) => {
+//     return array.reduce((acc, val) => acc + val)
+// }
+// const array = [10, 20, 40, 40]
+// console.log(massivSum(array));
+
+// 7
+// const maxNumber = (massiv) => {
+//     const sortArr = massiv.sort((a, b) => {
+//         return b - a
+//     })
+//     return sortArr[0]
+// }
+
+// console.log(maxNumber([10, 40, 540, 40, 640]))
+
+// 8
+// const factorialN = (n) => {
+//     let newValue = 1
+//     for (let i = 1; i <= n; i++) {
+//         newValue *= i
+//     }
+//     return newValue
+// }
+// console.log(factorialN(10));
+
+// 9
+// const ortaArifmetik = function(array = []) {
+//     const arifmetikNumber = array.reduce((acc,val) => {
+//         return acc + val
+//     })
+//     return arifmetikNumber / array.length
+// } 
+
+// console.log(ortaArifmetik([10, 20, 40, 70]));
+
+// 10d
+
+//  12
+// const sozSoni = (talk) => {
+//     const talkArr = talk.split(" ").length
+//     return talkArr
+
+// }
+// console.log(sozSoni("Hi my names Temur.My hobby is Coding. My work is frontend developer"));
+
+// 13
+// const customPow = function (number, degre) {
+//     let res = 1
+//     for (let i = 1; i <= degre; i++) {
+//         res *= number
+//     }
+//     return res
+// }
+// console.log(customPow(2, 3));
+
+// 14
+// const engUzunSoz = (text) => {
+//     const textArr = text.split(" ")
+//     let engUzunSoz = textArr[0]
+    
+//     for (let i of textArr) {
+//         if (engUzunSoz.length < i.length) {
+//             engUzunSoz = i
+//         } else if (engUzunSoz.length === i.length) {
+//             engUzunSoz = engUzunSoz
+//         }
+        
+//     }
+//     return `this talk is most long (${engUzunSoz}) `
+// }
+// console.log(engUzunSoz("Temur dasturchi ehtimoliy ekan"));
+// engUzunSoz("Temur dasturchi ekan")
+
+
+
+// 15
+// function bahoniAniqlash(ball) {
+//     if (ball >= 90 && ball <= 100) {
+//         return "A"
+//     } else if (ball >= 80 && ball < 90) {
+//         return "B"
+//     } else if (ball >= 70 && ball <= 80) {
+//         return "C"
+//     } else if (ball >= 60 && ball <= 70) {
+//         return "D"
+//     } else {
+//         return "you failed the exam !!!"
+//     }
+// }
+
+// console.log(bahoniAniqlash(69));
